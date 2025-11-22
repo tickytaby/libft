d242722e597646cad713385320655d3478ac51a3
Riccardo Yin <ryin@c3a6c1.42berlin.de>
2025-11-22 19:40:55 +0100

    ft_strtrim & libft.h

---
+++ ft_strtrim.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ryin <ryin@student.42berlin.de>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/22 17:15:30 by ryin              #+#    #+#             */
+/*   Updated: 2025/11/22 19:28:54 by ryin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+#include <stdio.h>
+
+int	in_set(char s, char const *set)
+{
+	while (*set)
+	{
+		if (*set == s)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
+int	f_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*out;
+	int	start;
+	int	end;
+	int	i;
+
+	start = 0;
+	while (in_set(s1[start++], set));
+	start--;
+	end = f_strlen(s1);
+	while (in_set(s1[--end], set) && end);
+	if (end < start)
+		return (NULL);
+	out = (char *)malloc(end - start + 2);
+	if (!out)
+		return (NULL);
+	i = 0;
+	while (start <= end)
+		out[i++] = s1[start++];
+	out[i] = 0;
+	return (out);
+}
+
+int main(void)
+{
+	const char *mystr = "zxy_hello world_xyz";
+	char *trimmed = ft_strtrim(mystr, "zxy_");
+	printf("OG: %s\nTrimmed: %s\n", mystr, trimmed);
+	const char *mystri = "zxy____";
+	char *trimd = ft_strtrim(mystri, "zxy_");
+	if (!trimd)
+		printf("Allocation failed\n");
+	else
+		printf("OG: %s\nTrimmed: %s\n", mystri, trimd);
+	return (0);
+}
